8bff2e8510c0ec9858c7e455c4793115ce575fef
Karthiban <karthikvcet007@gmail.com>
2020-02-18 18:40:53 +0530

    change

--- sitemanage/models.py
+++ sitemanage/models.py
@@ -1,28 +1,39 @@
 from django.db import models
 
 class SiteManage(models.Model):
-    siteid          = models.CharField(max_length=50,null=True,blank=True)
-    name            = models.CharField(max_length=50,null=True,blank=True)
-    clientName      = models.CharField(max_length=50,null=True,blank=True)
-    projectIncharge = models.CharField(max_length=50,null=True,blank=True)
-    typeOfProject   = models.CharField(max_length=50,null=True,blank=True)
-    siteEngineer    = models.CharField(max_length=50,null=True,blank=True)
-    startDate       = models.DateField(blank=True,null=True)
-    endDate         = models.DateField(blank=True,null=True)
-    lunchTime       = models.TimeField(blank=True,null=True)
-    startTime       = models.TimeField(blank=True,null=True)
-    endTime         = models.TimeField(blank=True,null=True)
-    strtBuffer      = models.TimeField(blank=True,null=True)
-    endBuffer       = models.TimeField(blank=True,null=True)
-    salaryStructure = models.CharField(max_length=50,null=True,blank=True)
+	site_id         = models.CharField(max_length=120, null=True, blank=True) 
+	name            = models.CharField(max_length=120, null=True, blank=True)
+	address         = models.CharField(max_length=120, null=True, blank=True)
+	client_name     = models.CharField(max_length=120, null=True, blank=True)
+	project_manager = models.CharField(max_length=120, null=True, blank=True)
+	project_type    = models.CharField(max_length=120,null=True, blank=True)
+	site_engineer   = models.CharField(max_length=120,null=True, blank=True)
+	start_date      = models.DateField(null=True, blank=True,)
+	end_date        = models.DateField(null=True, blank=True)
+	lunch_time      = models.TimeField(null=True, blank=True)
+	start_time      = models.TimeField(null=True, blank=True)
+	end_time        = models.TimeField(null=True, blank=True)
+	start_buffer    = models.TimeField(null=True, blank=True)
+	end_buffer      = models.TimeField(null=True, blank=True)
+	salary_structure = models.CharField(max_length=120,null=True, blank=True)
 
 
-from django.db.models.signals import post_save
 
-def id_create(sender,instance,created,*args,**kwargs):
-    if created:
-        instance.siteid = "site00"+str(instance.id)
-        instance.save()
+	def __str__(self):
+		return self.site_id
 
 
-post_save.connect(id_create,sender=SiteManage)
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+
+
+
+
+
+def id_creater(sender, instance, created,  *args, **kwargs):
+	if created:
+		instance.site_id = 'site00' + str(instance.id)
+		instance.save()
+
+
+post_save.connect(id_creater, sender=SiteManage)
--- sitemanage/views.py
+++ sitemanage/views.py
@@ -1,9 +1,54 @@
-# from django.shortcuts import render
-# from rest_framework.views import APIView
-# from rest_framwork.response import response
-# from .models import SiteManage
-# from .serializers import siteManageSerializers
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import SiteManageSerializer, SiteManageCreateSerializer
+from .models import SiteManage
+
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
+class SiteAPIView(viewsets.ViewSet):
+
+	def list(self, request):
+		data = SiteManage.objects.all()
+		serializer = SiteManageSerializer(data, many=True)
+		return Response(serializer.data)
+
+
+	def post(self, request, format=None):
+		serializer = SiteManageCreateSerializer(data=request.data)
+		print(request.data)
+		if serializer.is_valid():
+			serializer.save()
+			data = serializer.data
+			api = '/sitemanage/sites/{}/'.format(data.get('id'))
+			return Response({"satus":"created","data":serializer.data,"url":api})
+		return Response({"satus":"failed"})
+
+	def retrive(self, request, pk=None):
+	    queryset = SiteManage.objects.all()
+	    data = get_object_or_404(queryset, pk=pk)
+	    serializer = SiteManageSerializer(data)
+	    return Response(serializer.data)
 
 
 
 
+from rest_framework.decorators import api_view, permission_classes, renderer_classes, schema
+from django.views.decorators.csrf import csrf_exempt
+from labourer.models import LabourerManage
+
+ 
+@api_view(["POST"])
+def addLabourerToSiteAPIView(request):
+	try:
+		site_id = request.data.get("site_id")
+		labourer = request.data.get("labourer")
+		site = get_object_or_404(SiteManage, site_id=site_id)
+		for i in labourer:
+			lab = get_object_or_404(LabourerManage, labourerid=i)
+			lab.site_id = site_id
+			lab.save()
+		return Response({"satus":True,"message":"labours addedd"})
+	except:
+		return Response({"satus":False,"message":"internal error"})        
